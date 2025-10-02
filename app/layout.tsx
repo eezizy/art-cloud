2f86aada21f91c64e031bef7a3034c4e187c2d66
eezizy <161419484+eezizy@users.noreply.github.com>
2025-10-02 17:23:59 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="zh-CN">
-      <body style= margin: 0, fontFamily: "system-ui, -apple-system, Segoe UI, Roboto, Noto Sans, Arial" >
+      <body style= margin: 0, fontFamily: 'system-ui, -apple-system, "Segoe UI", Roboto, "Noto Sans", Arial' >
         {children}
       </body>
     </html>
